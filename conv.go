061345a8b4ffbeee02173ab570b328d12cb39dec
Dennis T Kaplan <gruessle@gmail.com>
2012-09-06 00:12:32 -0700

    just updated the docs

--- conv.go
+++ conv.go
@@ -15,6 +15,43 @@ type Char struct {
 	b byte
 }
 
+// Converts byte to character
+func B2c(s byte) string {
+	return string(s)
+}
+
+// Converts a character to a byte 
+func C2b(c string) byte {
+	b := NewString(c)
+	return b.GetByte()
+}
+
+// Converts a []byte list to string
+func Bl2s(b []byte) string {
+	return string(b)
+}
+
+// Converts a string to []byte 
+func S2bl(s string) []byte {
+	return []byte(s)
+}
+
+// Converts a string to int
+func S2i(s string) int {
+	i, _ := strconv.Atoi(s)
+	return i
+}
+
+// Converts a uint to a string
+func U2s(i uint) string {
+	return strconv.FormatUint(uint64(i), 10)
+}
+
+// Converts an int to a string
+func I2s(i int) string {
+	return strconv.Itoa(i)
+}
+
 // Set a Byte character
 func NewByte(c byte) *Char {
 	return &Char{c}
@@ -39,41 +76,4 @@ func (c *Char) GetString() string {
 // Returns an Int representation
 func (c *Char) GetInt() int {
 	return int(c.b)
-}
-
-// Converts byte to character
-func B2c(s byte) string {
-	return string(s)
-}
-
-// Converts a character to a byte 
-func C2b(c string) byte {
-	b := NewString(c)
-	return b.GetByte()
-}
-
-// Converts a []byte list to string
-func Bl2s(b []byte) string {
-	return string(b)
-}
-
-// Converts a string to []byte 
-func S2bl(s string) []byte {
-	return []byte(s)
-}
-
-// Doesn't work, returns the UTF-8 representation of the string in int
-func S2i(s string) int {
-	i, _ := strconv.Atoi(s)
-	return i
-}
-
-// Converts a uint to a string
-func U2s(i uint) string {
-	return strconv.FormatUint(uint64(i), 10)
-}
-
-// Converts an int to a string
-func I2s(i int) string {
-	return strconv.Itoa(i)
 }
