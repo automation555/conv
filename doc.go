8e76546f3b8adcd084a81544975cf2c93a2104ed
Dennis T Kaplan <gruessle@gmail.com>
2012-09-06 00:23:11 -0700

    Added doc.go

---
+++ doc.go
@@ -0,0 +1,11 @@
+// Converts characters from 
+//
+// character to byte 
+// string to []byte 
+// string to int 
+// []byte to string 
+// byte   to int
+// int    to string
+// ...
+
+package conv
